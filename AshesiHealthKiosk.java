70619e2c3ebd02e0c2e430fb8711e7fc4716d1cc
Tracy Phyron <tracy.jinor@ashesi.edu.gh>
2025-09-29 21:29:48 +0000

    Modified 1

--- AshesiHealthKiosk.java
+++ AshesiHealthKiosk.java
@@ -15,38 +15,47 @@ public class AshesiHealthKiosk {
                 break;
             case "T":
                 System.out.println(" Go to  Triage desk");
+                System.out.println("Welcome to the  Triage");
+                System.out.println("Kindly enter the a number ,1/2/3 for a health metric");
+                int healthMetric = systemService.nextInt();
+                switch(healthMetric){
+                    case 1:
+                        System.out.println("Kindly enter weight in kilograms");
+                        double weight = systemService.nextDouble();
+                        System.out.println("Kindly enter height in meters");
+                        double height= systemService.nextDouble();
+                        double bmi = weight/Math.pow(height,2);
+                        bmi = Math.round((bmi*10)/10.0);
+                        if (bmi < 18.5){
+                        System.out.println("you are underweight");
+                        }
+                        else if (bmi<=29.9 || bmi >= 25.0){
+                        System.out.println("you are overweight");
+                        }
+                        break;
+                        
+                    }
+                    
+                        
+
+
+        
+        
+       
+        
                 break;
             case "C" :
                 System.out.println(" Go to  to the Counseling desk");
             default:
                 System.out.println(" Invalid service code");
-        }
-        System.out.println("Kindly enter weight in kilograms");
-        double weight = systemService.nextDouble();
-        System.out.println("Kindly enter height in meters");
-        double height= systemService.nextDouble();
-        double bmi = weight/Math.pow(height,2);
-        bmi = Math.round((bmi*10)/10.0);
-        if (bmi < 18.5){
-        System.out.println("you are underweight");
-
-        }
-        else if (bmi<=24.9 || bmi >= 18.5){
-            System.out.println("your weight is normal");
-        }
-        else if (bmi<=29.9 || bmi >= 25.0){
-            System.out.println("you are overweight");
-
-        }
-        else {
-            System.out.println("you are obesed");
-        }
-
-        }
-
-
-
+            }
         
-
+  
+        }
 
     }
+
+      
+
+
+    
