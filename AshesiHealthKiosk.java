49c1c419d8901d2414d3e3bb6aaf3c4e7b74024b
Tracy Phyron <tracy.jinor@ashesi.edu.gh>
2025-09-29 18:17:23 +0000

    checkpoint2

--- AshesiHealthKiosk.java
+++ AshesiHealthKiosk.java
@@ -3,6 +3,27 @@ import java.util.Scanner;
 public class AshesiHealthKiosk {
     public static void main (String[] args){
         System.out.println("Welcome to the Ashesi university health kiosk");
+        Scanner systemService = new Scanner(System.in);
+        System.out.println("Enter one of the service codes P/L/T/C");
+        String Service = systemService.nextLine().toUpperCase();
+        switch(Service){
+            case "P":
+                System.out.println(" go to the Pharmacy desk");
+                break;
+            case "L":
+                System.out.println(" go to the Lab desk");
+                break;
+            case "T":
+                System.out.println(" go to the Triage desk");
+                break;
+            case "C" :
+                System.out.println(" go to the Counseling desk");
+            default:
+                System.out.println(" Invalid service code");
+                
+
+        }
+        
 
 
     }
